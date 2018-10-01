f396c60ad45f4b0df364684f459b9a8ced31c862
0h-n0 <kbu94982@gmail.com>
2018-10-01 14:06:00 +0900

    first

---
+++ module.py
@@ -0,0 +1,20 @@
+import torch
+
+
+
+class Encorder(torch.nn.Module):
+    def init(self):
+        pass
+
+class Decorder(torch.nn.Module):
+    def init(self):
+        pass
+
+class DiversityLayer(torch.nn.Module):
+    def init(self):
+        pass
+    
+
+class CDN(torch.nn.Module):
+    def init(self):
+        pass
