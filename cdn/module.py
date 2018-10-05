4a3813580413ce944e11a018a0b3d5c657ed1b2f
0h-n0 <kbu94982@gmail.com>
2018-10-05 13:46:05 +0900

    update:

--- cdn/module.py
+++ cdn/module.py
@@ -1,22 +1,25 @@
 import torch
 
+from .util import smiles_to_vec
 
 class SmilesEmbbeding(torch.nn.Module):
-    def init(self):
+    def __init__(self):
         pass
 
 class Encorder(torch.nn.Module):
-    def init(self):
+    def __init__(self):
         pass
 
 class Decorder(torch.nn.Module):
-    def init(self):
+    def __init__(self):
         pass
 
 class DiversityLayer(torch.nn.Module):
-    def init(self):
+    def __init__(self):
         pass
     
 class CDN(torch.nn.Module):
-    def init(self):
+    def __init__(self):
         pass
+
+    
