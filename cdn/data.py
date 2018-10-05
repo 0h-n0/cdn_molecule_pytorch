62fd4e24648f348b1d4882eb7c2d88de31fe3494
0h-n0 <kbu94982@gmail.com>
2018-10-05 14:01:14 +0900

    udpate:

--- cdn/data.py
+++ cdn/data.py
@@ -1,2 +1,18 @@
 
+class SmilesDataLoader(object):
+    def __init__(self, filename):
+        self.raw_data = None
+        with open(filename, 'r') as fp:
+            self.raw_data = fp.readlines()
+        print(self.raw_data)
+
+    def __getitem__(self, i):
+        pass
+
+if __name__ == '__main__':
+    SmilesDataLoader
+    
+
+
+
 
